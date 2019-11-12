25895f4317adab896dc2af537200c997f1eafcdc
尤从香 <ycx07807@ly.com>
2019-11-12 09:59:22 +0800

    优化problem 9

--- problem9/solution.go
+++ problem9/solution.go
@@ -2,15 +2,14 @@ package problem9
 
 //IsPalindrome 1
 func IsPalindrome(x int) bool {
-	if x < 0 {
+	if x < 0 || (x%10 == 0 && x != 0) {
 		return false
 	}
-	temp := x
 	rev := 0
-	for x > 0 {
+	for x > rev {
 		rev = rev*10 + x%10
 		x = x / 10
 	}
 
-	return rev-temp == 0
+	return x-rev == 0 || x-rev/10 == 0
 }
