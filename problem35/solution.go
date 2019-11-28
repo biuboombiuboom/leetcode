a6821a5aa8a2fbd07442e58bbbefd15c576864bf
尤从香 <ycx07807@ly.com>
2019-11-28 12:13:59 +0800

    35.搜索插入位置-二分

--- problem35/solution.go
+++ problem35/solution.go
@@ -2,6 +2,29 @@ package problem35
 
 //SearchInsert s
 func SearchInsert(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	if nums[right] < target {
+		return right + 1
+	}
+	for left <= right {
+		mid := (right + left) / 2
+		num := nums[mid]
+		if target == num {
+			return mid
+		}
+		if target > num {
+			left = mid + 1
+		}
+		if target < num {
+			right = mid - 1
+		}
+	}
+	return left
+}
+
+//SearchInsert1 s
+func SearchInsert1(nums []int, target int) int {
 	l := len(nums)
 	if l == 0 {
 		return 0
