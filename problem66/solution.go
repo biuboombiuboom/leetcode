5a41ef3a90b057d94967150de37d56e0c78bef9e
尤从香 <ycx07807@ly.com>
2019-12-03 19:44:31 +0800

    66.加一

---
+++ problem66/solution.go
@@ -0,0 +1,35 @@
+package problem66
+
+func PlusOne(digits []int) []int {
+	l := len(digits)
+	for i := l - 1; i > -1; i-- {
+		num := digits[i]
+		num++
+		if num < 10 {
+			digits[i] = num
+			return digits
+		}
+		digits[i] = num % 10
+	}
+
+	return append([]int{1}, digits...)
+
+}
+
+func PlusOne1(digits []int) []int {
+	l := len(digits)
+
+	if l == 0 {
+		return []int{1}
+	}
+	num := digits[l-1]
+	num++
+	if num >= 10 {
+		num = num - 10
+		digits = append(PlusOne(digits[0:l-1]), num)
+	} else {
+		digits[l-1] = num
+	}
+
+	return digits
+}
