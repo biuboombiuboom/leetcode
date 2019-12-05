cd966d5567c273905ae5fd2bc78a188692d0b4fa
尤从香 <ycx07807@ly.com>
2019-12-05 16:51:09 +0800

    6. Z 字形变换

--- nornal/algorithm/problem6.go
+++ nornal/algorithm/problem6.go
@@ -8,6 +8,30 @@ func Convert(s string, numRows int) string {
 		return s
 	}
 	sm := make([]string, numRows)
+	curRow := 0
+	down := false
+	for i := 0; i < len(s); i++ {
+		s1 := s[i : i+1]
+		sm[curRow] = sm[curRow] + s1
+		if curRow == 0 || curRow == numRows-1 {
+			down = !down
+		}
+		if down {
+			curRow++
+		} else {
+			curRow--
+		}
+
+	}
+	return strings.Join(sm, "")
+}
+
+//Convert1 c
+func Convert1(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+	sm := make([]string, numRows)
 
 	for i := 0; i < len(s); i++ {
 		s1 := s[i : i+1]
