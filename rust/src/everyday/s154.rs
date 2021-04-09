e70b25621a2c0b9ca4253fbbd4b680d24269c331
youcongxiang <ycx07807@ly.com>
2021-04-09 09:55:48 +0800

    s154

---
+++ rust/src/everyday/s154.rs
@@ -0,0 +1,10 @@
+pub fn find_min(nums: Vec<i32>) -> i32 {
+    let mut min=nums[0];
+    for i in 1..nums.len(){
+        if nums[i]<nums[i-1]{
+            min=nums[i];
+            break
+        }        
+    }
+    return min
+}
